ca0b0e3a0b3869b458096adeb06c0b07b0196327
zorro901 <zoro907@gmail.com>
2021-08-07 08:37:34 +0900

    49. 準備

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,6 @@
-import React, { useEffect, useState } from "react";
-import ColorfulMessage from "./compornents/colorfulMessage";
+import React from "react";
+import "./styles.css";
 
-const App = () => {
-  console.log("最初");
-  const [num, setNum] = useState(0);
-  const [faceShowFlag, setFaceShowFlag] = useState(false);
-
-  const onClickCountUp = () => {
-    setNum(num + 1);
-  };
-  const onClickSwithShowFlag = () => {
-    setFaceShowFlag(!faceShowFlag);
-  };
-
-  useEffect(() => {
-    if (num % 3 === 0 && num > 0) {
-      faceShowFlag || setFaceShowFlag(true);
-    } else {
-      faceShowFlag && setFaceShowFlag(false);
-    }
-    /* eslint-disable-next-line react-hooks/exhaustive-deps */
-  }, [num]);
-
-  return (
-    <>
-      <h1 style={{ color: "red" }}>こんにちは</h1>
-      <ColorfulMessage color="blue"> おげんきですか</ColorfulMessage>
-      <ColorfulMessage color="pink"> 元気です！</ColorfulMessage>
-      <button onClick={onClickCountUp}>カウントアップ</button>
-      <p>{num}</p>
-      <button onClick={onClickSwithShowFlag}>on/off</button>
-      {faceShowFlag && <p>🤔</p>}
-    </>
-  );
+export const App = () => {
+  return <div></div>;
 };
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
-import App from "./App";
+import { App } from "./App";
 
 // Appをコンポーネントとしてレンダリングする
 // 描画する位置を指定する
