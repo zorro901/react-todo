68337600dcd9fd4e90de0e328797ff335c1d81fd
zorro901 <zoro907@gmail.com>
2021-08-07 07:57:49 +0900

    45. Stateを知る(useState)

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorfulMessage from "./compornents/colorfulMessage";
 
 const App = () => {
-  const onClickButton = () => alert();
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const [num, setNum] = useState(0);
 
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは</h1>
       <ColorfulMessage color="blue"> おげんきですか</ColorfulMessage>
       <ColorfulMessage color="pink"> 元気です！</ColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <p>{num}</p>
     </>
   );
 };
