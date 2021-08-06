23cbdc7208c1a75d80bff99df1a4068aa5d6c181
zorro901 <zoro907@gmail.com>
2021-08-07 06:51:49 +0900

    41. コンポーネントの使い方を知る

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>こんにちは</h1>
+      <p>おげんきですか</p>
+    </>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
-
-const App = () => {
-  return (
-    <>
-      <h1>こんにちは</h1>
-      <p>おげんきですか</p>
-    </>
-  );
-};
+import App from "./App";
 
 // Appをコンポーネントとしてレンダリングする
 // 描画する位置を指定する
